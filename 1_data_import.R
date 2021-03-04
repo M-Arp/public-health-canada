fc24bdec5c81a50b1ebdd41c2585419519e8ece0
Simon Kiss <skiss@Simons-MacBook-Pro.local>
2021-03-03 23:20:29 -0500

    Uploading script with responses per day

--- 1_data_import.R
+++ 1_data_import.R
@@ -14,13 +14,15 @@ list.files(path="./data")
 #here returns the root directory
 here()
 #combining here() with 'data' and the file name returns the exact file path whether stored on a PC or Mac
-#Store as ph
-ph<-read_sav(here('data', 'ORD-571352-N3W7_Final_SPSS_022421.SAV'))
+#Store as full
+full<-read_sav(here('data', 'ORD-571352-N3W7_Final_SPSS_022421.SAV'))
+partial<-read_sav(here('data', 'ORD-571352-N3W7_Partial_SPSS_030221.SAV'))
 #Check structure
-str(ph)
+str(full)
 #View the data
-#View(ph)
+#View(full)
 #load the labelled library
 library(labelled)
 #check value labels
-val_labels(ph)
+val_labels(full)
+
