c5290d6e1df5d9146c38ec48ca4dffea21c3f76a
M-Arp <marp644@gmail.com>
2021-07-21 15:41:37 -0400

    Updated for review.

--- R_Scripts/3_bivariate_analyses.R
+++ R_Scripts/3_bivariate_analyses.R
@@ -1,19 +1,6 @@
 source('R_Scripts/2_data_preparation.R')
 theme_set(theme_bw())
 
-
-####TEST SCRIPTS####
-full$Sample
-lookfor(full, "federal government")
-names (full)
-  full %>%
-    select (contains('Q2'))
-  pivot_longer()
-  
-view()
-
-
-
 ###Q1 Most Important Problem#### 
 names(full)
 full %>% 
@@ -28,8 +15,6 @@ full %>%
   labs(y="", x="Percentage", title=str_wrap("Issue as Most Important Public Health Problem After COVID-19", width=60))+
   guides(fill=guide_legend(title=""))
 ggsave(here("Plots", "most_important_problem_group.png"), width=6, height=3)
-
-
 
 ##### Q2 Federal government managing COVID####
 lookfor(full, "federal government")
@@ -50,6 +35,11 @@ ggplot(full, aes(x=Q2_3,fill=Sample,..scaled..))+geom_density(alpha=0.5)+
   labs(y="", x="1=Very Poorly\n 7=Very Well", title= str_wrap("Satisfaction with Federal Government Managing Economic Disruptions", width=60))+
   guides(fill=guide_legend(title=""))
 ggsave(here("Plots", "Federal_Government_Satisfaction_Economic_Disruptions.png"))
+
+##### Q2 Significance Tests ####
+wilcox.test(Q2_1 ~ Sample, data=full)
+wilcox.test(Q2_2 ~ Sample, data=full)
+wilcox.test(Q2_3 ~ Sample, data=full)
 
 #### Q3-Q4 Influence Combined #### 
 lookfor(full, "influence")
@@ -72,9 +62,9 @@ full %>%
   scale_alpha_discrete(range =c(0.4, 1), limits=rev)+theme(legend.position = "bottom")+
   geom_errorbarh(aes(xmin=Percent-(1.96*error), xmax=Percent+(1.96*error)),height=0, position=position_dodge(0.9))+
   guides(fill=guide_legend(title=""))
-ggsave(here("Plots", "influences_do_should_policy_sample.png"), width=6, height=2)
+ggsave(here("Plots", "influences_do_should_policy_sample.png"))
 
-####Q3 Influences that are affecting government decision-making ####
+####Q3 Influences that should affecting government decision-making ####
 
 ###NEED TO FIX CONFIDENCE INTERVALS###
 
@@ -89,12 +79,14 @@ full %>%
   filter(value==1) %>% 
   as_factor() %>% 
   ggplot(., aes(y=name, x=Percent*100, fill=Sample, group=Sample))+
-  geom_col(position="dodge")+theme(legend.position = "bottom")+labs(y="", x="Percentage", title= str_wrap("Which of the following influences do you think are most affecting government decision-making in Canada about COVID-19", width=60))+geom_errorbarh(aes(xmin=Percent-(1.96*error), xmax=Percent+(1.96*error)),height=0, position=position_dodge(0.9))+
+  geom_col(position="dodge")+theme(legend.position = "bottom")+labs(y="", x="Percentage", title= str_wrap("Which of the following influences do you think should affect government decision-making in Canada about COVID-19", width=60))+geom_errorbarh(aes(xmin=Percent-(1.96*error), xmax=Percent+(1.96*error)),height=0, position=position_dodge(0.9))+
   guides(fill=guide_legend(title=""))
-ggsave(here("Plots", "influences_should_group.png"), width=6, height=2)
+ggsave(here("Plots", "influences_should_group.png"))
+
+###sIGNIFICANCE TESTING - Work in progress
 
 
-#### Q4 Influences that should affect government decision-making #### 
+#### Q4 Influences that are affecting government decision-making #### 
 
 ###NEED TO FIX CONFIDENCE INTERVALS###
 
@@ -110,9 +102,12 @@ full %>%
   filter(value==1) %>% 
   as_factor() %>% 
   ggplot(., aes(y=name, x=Percent*100, fill=Sample, group=Sample))+
-  geom_col(position="dodge")+theme(legend.position = "bottom")+labs(y="Influence", x="Percent", title= str_wrap("Which of the following influences do you think should affect government decision-making in Canada about COVID-19", width=60))+geom_errorbarh(aes(xmin=Percent-(1.96*error), xmax=Percent+(1.96*error)),height=0, position=position_dodge(0.9))+
+  geom_col(position="dodge")+theme(legend.position = "bottom")+labs(y="", x="Percentage", title= str_wrap("Which of the following influences do you think do affect government decision-making in Canada about COVID-19", width=60))+geom_errorbarh(aes(xmin=Percent-(1.96*error), xmax=Percent+(1.96*error)),height=0, position=position_dodge(0.9))+
   guides(fill=guide_legend(title=""))
-ggsave(here("Plots", "influences_do_group.png"), width=6, height=2)
+ggsave(here("Plots", "influences_do_group.png"))
+
+###sIGNIFICANCE TESTING - Work in progress
+
 
 #### Q5 Views on CMOH ####
 ####NEED TO MUTATE X AXIS *100 FOR %
@@ -121,7 +116,7 @@ full$Q5
 ggplot(full, aes(y=Sample, fill=as_factor(Q5)))+theme(legend.position = "bottom")+geom_bar(position="fill")+scale_fill_brewer(palette= "Set1", name="Role of Chief Medical Office of Health")+
   labs(y="", x="", title= str_wrap("In your view, which of the following best describes the role of your provincial Chief Medical Officer", width=60))+
   guides(fill=guide_legend(title="", ncol=2, byrow=TRUE))
-ggsave(here("Plots", "cmoh_role_group.png"), width=8, height=2)
+ggsave(here("Plots", "cmoh_role_group.png"))
 
 #### Q6 Trust in the Following ####
 lookfor(full, "trust")
@@ -136,9 +131,19 @@ full %>%
   summarize(average=mean(value), sd=sd(value), n=n(), se=sd/sqrt(n)) %>% 
   ggplot(., aes(x=name, y=average, col=Sample))+geom_point()+ylim(c(1,5))+
   geom_errorbar(aes(ymin=average-(1.96*se), ymax=average+(1.96*se)), width=0)+coord_flip() +
-  labs(y="1=Cannot be trusted at all\n 5=Can be trusted a lot", title=str_wrap("How much do you trust each of the following?", width=60), x="")
+  labs(y="1=Cannot be trusted at all\n 5=Can be trusted a lot", title=str_wrap("How much do you trust each of the following?", width=60), x="")+
   scale_x_discrete(labels = c("Chief Medical Officer of Health\nin your Province", "Chief Public Health Officer of Canada", "World Health Organization", "United States Centers of Disease\nControl and Prevention", "Chinese Centre for Disease Control\n and Prevention", "Provincial Government", "Federal Government"))
-ggsave(here('Plots', 'trust_groups.png'), width=6, height=2)
+ggsave(here('Plots', 'trust_groups.png'))
+
+####SIGNIFICANCE TEST
+wilcox.test(Q6_1 ~ Sample, data=full)
+wilcox.test(Q6_2 ~ Sample, data=full)
+wilcox.test(Q6_3 ~ Sample, data=full)
+wilcox.test(Q6_4 ~ Sample, data=full)
+wilcox.test(Q6_5 ~ Sample, data=full)
+wilcox.test(Q6_8 ~ Sample, data=full)
+wilcox.test(Q6_9 ~ Sample, data=full)
+
 
 #####  EXTRA Density plot for distribution of trust scores ####
 ggplot(full, aes(x=trust_average,fill=Sample,..scaled..))+geom_density(alpha=0.5)+
@@ -166,7 +171,17 @@ full %>%
   geom_errorbar(aes(ymin=average-(1.96*se), ymax=average+(1.96*se)), width=0)+coord_flip() +
   labs(y="1=Very poorly \n 5=Very well", x="", title= str_wrap("How would you rate the following groups in preventing the spread of COVID-19?", width=60)) +
   scale_x_discrete(labels = c("Young People", "Your Neighbours", "Senior Citizens", "Federal Government", "Provincial Government", "Municipal Government", "School Boards", "Public Health Professionals"))
-ggsave(here('Plots', 'groups_preventing_spread.png'), width=6, height=2)
+ggsave(here('Plots', 'groups_preventing_spread.png'))
+
+####Significance Testing
+wilcox.test(Q7_1 ~ Sample, data=full)
+wilcox.test(Q7_2 ~ Sample, data=full)
+wilcox.test(Q7_3 ~ Sample, data=full)
+wilcox.test(Q7_4 ~ Sample, data=full)
+wilcox.test(Q7_5 ~ Sample, data=full)
+wilcox.test(Q7_6 ~ Sample, data=full)
+wilcox.test(Q7_7 ~ Sample, data=full)
+wilcox.test(Q7_8 ~ Sample, data=full)
 
 
 #### Q8 Difference Between Samples and Support For Measures #####
@@ -179,6 +194,31 @@ full %>%
   labs(title="Support For Interventions", x="Intervention", y="")+
   guides(fill=guide_legend(title=""))
 ggsave(here('Plots', 'Interventions_by_sample.png'))
+
+lookfor(full, "support or oppose")
+full$Q8_1
+ggplot(full, aes(x=Q8_1,fill=Sample,..scaled..))+geom_density(alpha=0.5)+ 
+  labs(y="", x="1=Strongly oppose\n 7=Strongly support", title= str_wrap ("It should be mandatory for all residents in Canada to get a vaccine that has been approved by Health Canada", width = 60))+
+  guides(fill=guide_legend(title=""))
+ggsave(here("Plots", "Support_mandatory_vaccines.png"))
+
+full$Q8_2
+ggplot(full, aes(x=Q8_2,fill=Sample,..scaled..))+geom_density(alpha=0.5)+ 
+  labs(y="", x="1=Strongly oppose\n 7=Strongly support", title= str_wrap ("Bars and restaurants should be closed down", width = 60))+
+  guides(fill=guide_legend(title=""))
+ggsave(here("Plots", "Support_bars_closed.png"))
+
+full$Q8_3
+ggplot(full, aes(x=Q8_3,fill=Sample,..scaled..))+geom_density(alpha=0.5)+ 
+  labs(y="", x="1=Strongly oppose\n 7=Strongly support", title= str_wrap ("People not wearing masks in public, indoor places should be fined", width = 60))+
+  guides(fill=guide_legend(title=""))
+ggsave(here("Plots", "Support_mask_fines.png"))
+
+##SIGNIFICANCE TEST
+wilcox.test(Q8_1 ~ Sample, data=full)
+wilcox.test(Q8_2 ~ Sample, data=full)
+wilcox.test(Q8_3 ~ Sample, data=full)
+
 
 #### Correlation between Vaccine Severity and measures - NOT WORKING #### 
 
@@ -213,7 +253,13 @@ full %>%
   ggplot(., aes(x=name, y=average, col=Sample))+geom_point()+ylim(c(1,10))+
   geom_errorbar(aes(ymin=average-(1.96*se), ymax=average+(1.96*se)), width=0)+coord_flip()+
   labs(y="1=Stopping the spread of COVID-19 \n 10=Other considerations", x="", title= str_wrap("In public health, it is often important to decide between accomplishing multiple outcomes, which outcome is more important to you ", width=60)) +
-ggsave(here('Plots', 'trade_off_group.png'), width=6, height=2)
+ggsave(here('Plots', 'trade_off_group.png'))
+
+lookfor(full, "decline")
+wilcox.test(social_isolation ~ Sample, data=full)
+wilcox.test(seniors_isolation ~ Sample, data=full)
+wilcox.test(schools_open ~ Sample, data=full)
+wilcox.test(decline_economy ~ Sample, data=full)
 
 ####Q23 Vaccine Hesitancy ####
 lookfor(full, "eligible")
@@ -226,19 +272,19 @@ lookfor(full, "eligible")
 lookfor(full, "greater powers")
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q24_1)))+geom_bar(position="fill")+labs(x="", y="")+scale_fill_manual(values = c("dark red", "tomato", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Would you disagree or agree to giving the federal government greater power to do the following even if it would mean acting in an area of provincial responsibility: Monitoring the spread of diseases", width = 60 )) +
   guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "Greater_Federal_Powers_Monitoring.png"), width=6, height=2)
+ggsave(here("Plots", "Greater_Federal_Powers_Monitoring.png"))
 
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q24_2)))+geom_bar(position="fill")+labs(x="", y="")+scale_fill_manual(values = c("dark red", "tomato", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Would you disagree or agree to giving the federal government greater power to do the following even if it would mean acting in an area of provincial responsibility: Emergency response", width = 60 )) +
   guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "Greater_Federal_Powers_Emergency.png"), width=6, height=2)
+ggsave(here("Plots", "Greater_Federal_Powers_Emergency.png"))
 
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q24_3)))+geom_bar(position="fill")+labs(x="", y="")+scale_fill_manual(values = c("dark red", "tomato", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Would you disagree or agree to giving the federal government greater power to do the following even if it would mean acting in an area of provincial responsibility: Control over vaccine supply chain", width = 60 )) +
   guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "Greater_Federal_Powers_Vaccines.png"), width=6, height=2)
+ggsave(here("Plots", "Greater_Federal_Powers_Vaccines.png"))
 
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q24_4)))+geom_bar(position="fill")+labs(x="", y="")+scale_fill_manual(values = c("dark red", "tomato", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Would you disagree or agree to giving the federal government greater power to do the following even if it would mean acting in an area of provincial responsibility: Population mental health", width = 60 )) +
   guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "Greater_Federal_Powers_Mental_Health.png"), width=6, height=2)
+ggsave(here("Plots", "Greater_Federal_Powers_Mental_Health.png"))
 
 
 
@@ -268,19 +314,19 @@ gsave(here("Plots", "science_policy_group.png"))
 ###NEED TO RESCALE Y Axis *100 to be a percentage ###
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q32)))+geom_bar(position="fill")+
   labs(y="", x="")+scale_fill_manual(values = c("dark red", "tomato", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Politicians are ready to lie to get elected", width = 60 )) +guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "trust_politicians_lie_group.png"), width=6, height=2)
+ggsave(here("Plots", "trust_politicians_lie_group.png"))
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q33)))+geom_bar(position="fill")+
   labs(y="", x="")+scale_fill_manual(values = c("dark red", "tomato", "gray", "royal blue", "dark blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("How much of the time do you think you can trust the government in Ottawa to do what is right", width = 60 ))+guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "trust_ottawa_group.png"), width=6, height=2)
+ggsave(here("Plots", "trust_ottawa_group.png"))
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q34)))+geom_bar(position="fill")+
   labs(y="", x="")+scale_fill_manual(values = c("tomato", "gray", "royal blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Do you think that people in government waste:", width = 60 ))+guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "trust_taxes_group.png"), width=6, height=2)
+ggsave(here("Plots", "trust_taxes_group.png"))
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q35)))+geom_bar(position="fill")+
   labs(y="", x="")+scale_fill_manual(values = c("tomato", "royal blue") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Would you say the government is...", width = 60 ))+guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "trust_interests_group.png"), width=10, height=2)
+ggsave(here("Plots", "trust_interests_group.png"))
 ggplot(full, aes(y=as.factor(Sample), fill=as_factor(Q36)))+geom_bar(position="fill")+
   labs(y="", x="")+scale_fill_manual(values = c("royal blue", "tomato") ,(name=""))+theme(legend.position = "bottom")+labs (title= str_wrap("Generally speaking, would you say that most people can be trusted", width = 60 ))+guides(fill = guide_legend(reverse=TRUE))
-ggsave(here("Plots", "trust_people_group.png"), width=6, height=2)
+ggsave(here("Plots", "trust_people_group.png"))
 
 ####Q37 Individualism #####
 lookfor(full, "society")
@@ -305,8 +351,6 @@ ggplot(full, aes(x=Q37_4,fill=Sample,..scaled..))+geom_density(alpha=0.5)+
 ggsave(here("Plots", "Private_sector_jobs.png"))
 
 ####Q38 Equality & Discrimination####
-
-#START HERE#
 
 lookfor(full, "society")
 ggplot(full, aes(x=Q38_1,fill=Sample,..scaled..))+geom_density(alpha=0.5)+
